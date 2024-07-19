e71d63185d5593181ed007bbbe158b449531bc67
Richard <richardcortez934@gmail.com>
2024-07-18 21:33:37 -0400

    Modify some filds in user.model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,4 @@
-import { DataType, DataTypes } from "sequelize";
+import { DataTypes } from "sequelize";
 
 const UserModel = {
     id: {
@@ -8,18 +8,23 @@ const UserModel = {
     },
     usuario: {
         type: DataTypes.STRING(50),
+        unique: true,
     },
     clave: {
         type: DataTypes.STRING(400),
+        allowNull: false,
     },
     rol_id: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        allowNull: false,
     },
     nombre: {
         type: DataTypes.STRING(50),
+        allowNull: false,
     },
     apellido: {
         type: DataTypes.STRING(50),
+        allowNull: false,
     },
     status:{
         type: DataTypes.BOOLEAN,
